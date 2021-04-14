47c8dc55a6e09e0f81c64b2890444e3a4595a458
Vsevolod Velichko <torkvema@gmail.com>
2021-04-15 02:01:46 +0300

    add on-off switch for dimmable lights

--- dialogs/devices/wirenboard/dimmable_light.py
+++ dialogs/devices/wirenboard/dimmable_light.py
@@ -8,7 +8,7 @@ import logging
 from dialogs.mqtt_client import MqttClient
 
 from dialogs.protocol.device import Light
-from dialogs.protocol.capability import Range
+from dialogs.protocol.capability import Range, OnOff
 
 
 class WbDimmableLight(Light):
@@ -25,6 +25,10 @@ class WbDimmableLight(Light):
         room=None,
     ):
         self.client = mqtt_client
+        self.onoff = OnOff(
+            change_value=self.change_onoff,
+            retrievable=True,
+        )
         self.level = Range(
             change_value=self.change_level,
             retrievable=True,
@@ -35,6 +39,8 @@ class WbDimmableLight(Light):
             precision=1. if (range_high - range_low) < 500 else 0.1,
         )
 
+        self.last_val = 100.
+
         self.range_low = range_low
         self.range_high = range_high
         self.status_path = status_path
@@ -43,7 +49,7 @@ class WbDimmableLight(Light):
 
         super().__init__(
             device_id=device_id,
-            capabilities=[self.level],
+            capabilities=[self.onoff, self.level],
             device_name=name,
             description=description,
             room=room,
@@ -52,8 +58,11 @@ class WbDimmableLight(Light):
         )
 
     async def on_level_changed(self, topic: str, payload: str) -> None:
-        percent_value = (int(payload) - self.range_low) / (self.range_high - self.range_low) * 100.
+        percent_value = max(0, (int(payload) - self.range_low) / (self.range_high - self.range_low) * 100.)
         self.level.value = percent_value
+        self.onoff.value = percent_value > 0
+        if percent_value > 0:
+            self.last_val = percent_value
 
     async def change_level(
         self,
@@ -64,5 +73,18 @@ class WbDimmableLight(Light):
     ) -> typing.Tuple[str, str]:
         real_value = str(int(value / 100 * (self.range_high - self.range_low) + self.range_low))
         logging.getLogger('wb.dimlight').info("Switching light to %s (real value %s)", value, real_value)
+        self.client.send(self.control_path, real_value)
+        return (capability.type_id, instance)
+
+    async def change_onoff(
+        self,
+        device: "WbDimmableLight",
+        capability: OnOff,
+        instance: str,
+        value: bool,
+    ):
+        target = self.last_val if value else 0.
+        real_value = str(int(target / 100 * (self.range_high - self.range_low) + self.range_low))
+        logging.getLogger('wb.dimlight').info("Switching light to %s (real value %s)", target, real_value)
         self.client.send(self.control_path, real_value)
         return (capability.type_id, instance)
